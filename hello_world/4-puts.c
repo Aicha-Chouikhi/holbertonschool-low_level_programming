d25c8d68ba03f8fb806d2f9598e53ae516bdf7d2
root <chouikhi_aicha@live.fr>
2023-02-17 03:53:11 -0800

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #!/bin/bash
 #include <stdio.h>
 
+/** 
+ *main - Entery point
+ *Return: Always 0 (Success) 
+ */
+
 int main() {
     puts("Programming is like building a multilingual puzzle\n");
     return 0;
