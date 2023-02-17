e0e6f311c56951c78b67ca78f5a483378b1bef59
root <chouikhi_aicha@live.fr>
2023-02-17 10:10:46 -0800

    task6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-
 /**
 * main - Entery point
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-printf("Size of a char: %1d byte(s) \n", sizeof(char));
-printf("Size of an int: %4d byte(s) \n", sizeof(int));
-printf("Size of a long int: %4d byte(s) \n", sizeof(long int));
-printf("Size of a long long int: %8d byte(s) \n", sizeof(long long int));
-printf("Size of a float: %4d byte(s) \n", sizeof(float));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
