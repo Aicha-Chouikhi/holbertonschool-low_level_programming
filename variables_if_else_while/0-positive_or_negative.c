0bee717a6525eed983f6343eb9316ecb5872337f
root <chouikhi_aicha@live.fr>
2023-02-20 02:54:47 -0800

    task0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -6,6 +6,9 @@
 int main(void)
 {
 int n;
+ 
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 if (n > 0) {
 printf("%d is positive\n", n);
 } else if (n == 0) {
@@ -13,8 +16,6 @@ printf("%d is zero\n", n);
 } else {
 printf("%d is negative\n", n);
 }
-srand(time(0));
-n = rand() - RAND_MAX / 2;
 /* your code goes there */
 return (0);
 }
