42ff6e258079d60cf6590081202b3a1b022d47fd
root <chouikhi_aicha@live.fr>
2023-02-21 02:27:14 -0800

    task 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
@@ -15,7 +14,7 @@ int main(void)
 char x;
 for(char x = 'a'; x <= 'z'; x++);
 putchar(x);
-}
+ 
 putchar('\n');
 return (0);
 }
