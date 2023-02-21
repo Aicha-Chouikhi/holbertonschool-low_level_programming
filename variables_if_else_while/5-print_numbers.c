8af4e56d2adaffa5c9611a5a953c524a239ab4fb
root <chouikhi_aicha@live.fr>
2023-02-21 05:32:19 -0800

    task5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* print digit*
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x;
+
+for (int x = 0; x < 10; x++)
+printf("%d\n", x);
+
+return (0);
+}
