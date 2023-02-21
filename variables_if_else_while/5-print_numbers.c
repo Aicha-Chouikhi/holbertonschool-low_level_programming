09c6b5397d51b5a91326b4ebbd4da28ab2b62de5
root <chouikhi_aicha@live.fr>
2023-02-21 06:16:54 -0800

    task5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -10,14 +10,12 @@
 
 int main(void)
 {
-int a = 0;
+	int a = 0;
 
-for (a = 0; a <= 9; a++)
-{
-  putchar(a + '0');
- }
- 
-putchar('\n');
-
-return (0);
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
+	return (0);
 }
