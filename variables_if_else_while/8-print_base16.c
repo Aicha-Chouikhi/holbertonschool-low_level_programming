a93dc9ee2e793be3e2ba41b887ac8033c00e439f
root <chouikhi_aicha@live.fr>
2023-02-22 00:53:15 -0800

    task8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* print the base of 16 *
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char x;
+for(x='0'; x<='9'; x++)
+putchar(x);
+
+for(x='a'; x<='f'; x++)
+putchar(x);
+
+putchar('\n');
+return (0);
+}
