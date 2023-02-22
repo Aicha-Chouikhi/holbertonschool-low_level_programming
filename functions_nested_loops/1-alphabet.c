09340e9ef33fd4f33dfd527c8300782f21fb5c8d
root <chouikhi_aicha@live.fr>
2023-02-22 11:20:02 -0800

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,24 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* print the alphabet in lowercase *
+*
+* Return: Always 0 (Success)
+*/
 void print_alphabet(void)
-char letter = 'a';
-while (letter <= 'z')
-_putchar(letter);
-letter++;
+{
+char alph = 'a';
+while (alph <= 'z')
+_putchar(alph);
+alph++;
 
 _putchar('\n');
+}
+
+int main(void)
+{
+print_alphabet();
+return 0;
+
+}
