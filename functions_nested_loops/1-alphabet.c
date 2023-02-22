eac1de00d50570153781f12ae8d902b85f9c2555
root <chouikhi_aicha@live.fr>
2023-02-22 13:48:27 -0800

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
-* main - check the code for Holberton School students.
+* print_alphabet -print alphabet
 *
-* Return: Always 0.
+* Return: void
 */
 void print_alphabet(void)
 {
