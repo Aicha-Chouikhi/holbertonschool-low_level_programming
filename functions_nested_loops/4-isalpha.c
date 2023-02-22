cc74c36180c724541af9d44ec871ceb44dd98c68
root <chouikhi_aicha@live.fr>
2023-02-22 15:43:36 -0800

    task4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* function that checks for alphabetic character
+*
+* Return: void
+*/
+int _isalpha(int c);
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return 1;
+else
+return 0;
+}
