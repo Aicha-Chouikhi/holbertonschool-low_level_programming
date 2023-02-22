be0fef80370ebaaf7028ac56de0cc609992add3f
root <chouikhi_aicha@live.fr>
2023-02-22 15:01:25 -0800

    task2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,11 @@
 */
 void print_alphabet_x10(void)
 {
-int i, x;
-i = 0;
-while (i < 10);
-for (x = 'a'; x <= 'z'; x++)
-_putchar(x);
-i++,
+int i, n;
 
-putchar('\n');
+for (i = 0; i < 10; i++)
+for (n = 'a'; n <= 'z'; n++)
+_putchar(n);
+
+_putchar('\n');
 }
