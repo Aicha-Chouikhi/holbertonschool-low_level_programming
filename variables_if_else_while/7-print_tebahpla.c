b8cceccc0b9c7aebed1e71179e84a9ef98b0c1c5
root <chouikhi_aicha@live.fr>
2023-02-22 00:36:20 -0800

    task7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* print the alphabet in reverse *
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+
+for (i = 'z'; i >= 'a'; i--)
+putchar(i);
+
+putchar('\n');
+return (0);
+}
