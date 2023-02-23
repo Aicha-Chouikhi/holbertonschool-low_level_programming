ba34e5e835940fdec67e57bbc9f99fba2ba1e563
root <chouikhi_aicha@live.fr>
2023-02-23 08:17:58 -0800

    task7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,11 @@
 */
 int print_last_digit(int n)
 {
-  int j = n % 10;
+int j = n % 10;
 
-  if (j < 0)
-    j = j * -1;
+if (j < 0)
+j = j * -1;
 _putchar(j + '0');
 
- return (j);
+return (j);
 }
