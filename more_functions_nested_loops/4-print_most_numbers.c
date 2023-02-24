b2d0599dde3a3d8c0620280af47fa5d4dc5e71f2
root <chouikhi_aicha@live.fr>
2023-02-24 03:40:59 -0800

    task 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - a function that prints the numbers, from 0 to 9 except 2,4
+ * print_most_numbers - a function that prints 0-9 except 2,4
  * Return: void
  */
 void print_most_numbers(void)
