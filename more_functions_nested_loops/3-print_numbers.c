ae225f44a3aa31e572eaa91e7927d393be1d442a
root <chouikhi_aicha@live.fr>
2023-02-24 02:25:17 -0800

    task 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,13 @@
 
 /**
  * print_numbers - a function prints numbers
- * @i: number input
+ * @i: counter
  * Return: numbers from 0 to 9
  */
 void print_numbers(void)
 {
 int i;
+ 
 for (i = 0; i < 10; i++)
 _putchar(i + '0');
 _putchar('\n');
