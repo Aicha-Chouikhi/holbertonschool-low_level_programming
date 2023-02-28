f97cfeb95cf20e1eaa27ca7fe368aa70b6248edd
root <chouikhi_aicha@live.fr>
2023-02-27 23:25:42 -0800

    task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - is a function that prints a string
+ * @str: input
+ *  Return: Always 0.
+ */
+void _puts(char *str)
+{
+
+int i = 0;
+for (str [i] != "\0", i++)
+{
+printf (%c, str[i]);
+}
+printf ("\n");
+}
