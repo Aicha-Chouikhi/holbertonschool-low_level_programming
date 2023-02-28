6bc38bad63bb999b147fcf07999c1b17112c39a1
root <chouikhi_aicha@live.fr>
2023-02-28 00:05:11 -0800

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string, in reverse
+ * @s: string input
+ * Return: string in reverse
+ */
+
+void print_rev(char *s)
+{
+
+int k = 0;
+int i, n;
+for (i = 0; s[i] != '\0'; i++)
+{
+k++;
+}
+for (n = (k - 1); n >= 0; n--)
+{
+_putchar(s[n]);
+}
+_putchar('\n');
+}
