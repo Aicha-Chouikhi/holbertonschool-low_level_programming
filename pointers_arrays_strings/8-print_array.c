c1e9abfdaacc0961e3dd84891609a29a73327a7f
root <chouikhi_aicha@live.fr>
2023-02-28 07:28:59 -0800

    task 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * print_array - Prints an inputted number of an array
@@ -10,12 +11,12 @@
 void print_array(int *a, int n)
 
 {
-int i;
+int i = 0;
 for (i = 0; i < n; i++)
 {
 printf("%d", a[i]);
 if (i < n - 1)
 printf(", ");
 }
-printf("\n");
+_putchar('\n');
 }
