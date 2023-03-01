37ca735358e466d20376c247cbd36fd05937d5c4
root <chouikhi_aicha@live.fr>
2023-03-01 01:49:35 -0800

    2task 1

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -13,7 +13,9 @@ char *_strcat(char *dest, char *src)
 int i, n;
 for (i = 0; dest[i] != '\0'; i++)
 {
-for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
 }
- return (dest);
+for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+{
+}
+return (dest);
 }
