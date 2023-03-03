40b55afadba892980762d70f0c118a3d3c567319
root <chouikhi_aicha@live.fr>
2023-03-03 09:35:35 -0800

    3task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - is a function that locates a substring
+ * @haystack: input
+ * @needle: input
+ * Return: string or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i, j;
+
+for (i = 0; haystack[i] != '\0'; i++)
+{
+for (j = 0; needle[j] != '\0' && haystack[i+j] == needle[j]; j++)
+if (needle[j] == '\0')
+{
+return (&haystack[i]);
+}
+}
+return (NULL);
+}
