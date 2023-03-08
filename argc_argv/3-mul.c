e226aae4cb00c0ffc16ffb67b27dc19205958051
root <chouikhi_aicha@live.fr>
2023-03-08 13:55:50 -0800

    task3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,13 +10,14 @@
  */
 int main(int argc, char *argv[])
 {
+int i, res = 1;
 if (argc != 3)
 {
-printf("Error\n");
+printf("%s\n", "Error");
 return (1);
 }
 else
-{	
+{
 for (i = 1; i < argc; i++)
 {
 res *= atoi(argv[i]);
