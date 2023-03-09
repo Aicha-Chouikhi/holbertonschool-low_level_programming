8d7949da4712193833ab076b9fa3756f59f9f7b7
root <chouikhi_aicha@live.fr>
2023-03-09 05:48:33 -0800

    static

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,3 +1,7 @@
+#ifdef MATH_H
+#define MATH_H
+
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +22,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif /* MATH_H */
